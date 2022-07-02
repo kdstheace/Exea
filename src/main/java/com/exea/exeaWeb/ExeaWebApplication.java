d0ee026c42ffe0cdd84d6a787d9d470783724671
Dongsoo Kim <william@uzen.io>
2022-07-03 07:59:21 +0900

    exea setting
    exea setting

---
+++ src/main/java/com/exea/exeaWeb/ExeaWebApplication.java
@@ -0,0 +1,11 @@
+package com.exea.exeaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeaWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExeaWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/exea/exeaWeb/ExeaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exea.exeaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
