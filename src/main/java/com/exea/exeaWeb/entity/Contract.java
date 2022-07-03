32998473ae578318cee5a819287339d9acf79f79
DongsooWilliamKim <kdstheace@naver.com>
2022-07-03 20:12:05 +0900

    setting local db and connection
    setting local db and connection

---
+++ src/main/java/com/exea/exeaWeb/entity/Contract.java
@@ -0,0 +1,20 @@
+package com.exea.exeaWeb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="contracts")
+public class Contract {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
